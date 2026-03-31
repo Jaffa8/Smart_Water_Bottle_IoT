d6e7a6a9d71e8feb94292afe1f9e6ae85f525c03
Sachit Parmar <127434651+Jaffa8@users.noreply.github.com>
2026-03-31 11:35:55 +0530

    Changed water level and status handling logic

--- sketch.ino
+++ sketch.ino
@@ -18,63 +18,63 @@ BlynkTimer timer;
 unsigned long lastSipTime = 0;
 int lastWaterLevel = 100;
 
-const unsigned long SIP_REMINDER_TIME = 10000; // 10 seconds for testing
+const unsigned long SIP_REMINDER_TIME = 10000; 
 
 void checkBottle() {
-  // 1. Water Level calculation
+  
   digitalWrite(TRIG_PIN, LOW); delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH); delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
   long duration = pulseIn(ECHO_PIN, HIGH);
   float distance = duration * 0.034 / 2; 
   
-  =
+ 
   int waterLevel = map(distance, 5, 350, 100, 0);
   if(waterLevel > 100) waterLevel = 100;
   if(waterLevel < 0) waterLevel = 0;
 
-  =
+ 
   int16_t ax, ay, az, gx, gy, gz;
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
   float tiltAngle = atan2(ax / 16384.0, az / 16384.0) * 180.0 / PI;
 
   String status = "";
 
-  =
+
   
   if (abs(tiltAngle) > 45.0) { 
-   
+  
     status = "Drinking...";
     lastSipTime = millis(); 
     lastWaterLevel = waterLevel;
   } 
   else if (waterLevel <= 0) {
-    
+   
     status = "Fill the water bottle!";
     lastWaterLevel = waterLevel;
   }
   else if (waterLevel >= 100) {
-    
+  
     status = "Water bottle filled!";
     lastWaterLevel = waterLevel;
     lastSipTime = millis(); 
   }
   else if (waterLevel < lastWaterLevel) {
-    
+   
     status = "You just had a sip!";
     lastSipTime = millis();
     lastWaterLevel = waterLevel;
   }
   else if (millis() - lastSipTime < 10000 && lastSipTime != 0) {
-    
+   
     status = "You just had a sip!";
   }
   else if (millis() - lastSipTime > SIP_REMINDER_TIME) {
-    
+  
     status = "Time to hydrate! Take a sip";
   }
   else {
-    
+  
     status = "Hydrated";
   }
 
